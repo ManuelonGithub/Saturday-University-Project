b4d931bd20c8e2b442172664d9cef8c185c6ecdb
ManuelonGit <manuelonemail@gmail.com>
2017-11-21 21:19:07 -0400

    WIP of the course input file reading

--- Course file read test/course-input-file-read.cpp
+++ Course file read test/course-input-file-read.cpp
@@ -8,13 +8,14 @@ using namespace std;
 
 class course
 {
-	private: 
+	private:
 			string courseID;
 			vector<string> pre_req;
-	
+
 	public:
 			course(string cID = " ")	{ courseID = cID; }
 			void set_pre_req(string id)	{ pre_req.push_back(id); }
+			void write(ostream &out) const;
 };
 
 void courses_read(string Filepath, vector<course> &courses, int &total_courses_given)
@@ -22,44 +23,59 @@ void courses_read(string Filepath, vector<course> &courses, int &total_courses_g
 	course *c;
 	string str;
 	ifstream courses_in;
-	
+
 	courses_in.open(Filepath.c_str());
-	
+
 	if(!courses_in)
 	{
 		cout << "error opening courses input file. Make sure the name of the file is: courses.txt" << endl;
-		
+
 		exit(0);
 	}
 	else
 	{
 		courses_in >> total_courses_given;
-		
+
 		while(getline(courses_in, str))
 		{
 			istringstream ss(str);
 			string id;
-			
+
 			ss >> id;
-			c = new course(id); 
-			
+			c = new course(id);
+
 			while(ss >> id)
 			{
 				c->set_pre_req(id);
 			}
-			
-			courses.push_back(c);
+
+			courses.push_back(*c);
 		}
-		
+	}
+
 }
 
 int main()
 {
 	vector<course> courses;
-	
+
 	int total_courses_given;
 
 	courses_read("courses.txt",courses, total_courses_given);
-	
+
+	for(int i = 0; i <= total_courses_given; i++)
+	{
+		courses[i].write(cout);
+	}
+
 	return 0;
+}
+
+void course::write(ostream &out) const
+{
+    out << courseID << ": " << endl;
+    for(vector<string>::const_iterator i = pre_req.begin(); i != pre_req.end(); ++i)
+    {
+        out << "\t" << *i << endl;
+    }
 }
