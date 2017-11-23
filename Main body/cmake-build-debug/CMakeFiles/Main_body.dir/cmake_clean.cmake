f8067f8779b1b6f7b313750a093499895d54ed96
ManuelonGit <manuelonemail@gmail.com>
2017-11-22 22:47:32 -0400

    Added some files that my IDE needs to work properly

---
+++ Main body/cmake-build-debug/CMakeFiles/Main_body.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Main_body.dir/Sat-Uni-Main-Body.cpp.o"
+  "CMakeFiles/Main_body.dir/course.cpp.o"
+  "CMakeFiles/Main_body.dir/systemINI.cpp.o"
+  "CMakeFiles/Main_body.dir/classroom.cpp.o"
+  "CMakeFiles/Main_body.dir/student.cpp.o"
+  "Main_body.pdb"
+  "Main_body.exe"
+  "libMain_body.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Main_body.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
