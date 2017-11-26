37629db672d2339d199b679e908675d979f6f729
Ibrahim Fatungase <ibrahimfatungase@TF454.WPA.Dal.Ca>
2017-11-26 14:15:23 -0400

    my main cpp ibrah

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  satuni
+//
+//  Created by Ibrahim Fatungase on 2017-11-25.
+//  Copyright © 2017 Ibrahim Fatungase. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
