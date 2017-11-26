99ae00b31d0c4f600518c276d1afa58acfa5da83
CallumCottrell <callum.cottrell@mac.com>
2017-11-25 20:46:44 -0400

    working FUS

--- System Files/Sat-Uni-Main-Body.cpp
+++ System Files/Sat-Uni-Main-Body.cpp
@@ -10,27 +10,30 @@ using namespace std;
 int main()
 {
     reg_system sys;
-    vector<course> courses; 
-    vector<classroom> classrooms; 
+    vector<course> courses;
+    vector<classroom> classrooms;
     vector<student> students;
-
+    vector<string> FUS;
+    
     sys.create("system-parameters-in.txt");
-
+    
     //sys.write(cout);
-
-    //courses_read("courses.txt", courses, sys);
-
+    
+    courses_read("courses.txt", courses, sys);
+    
+    
     //print_all_courses(cout, courses);
-
-    classrooms_read("classrooms.txt", classrooms, sys);
-
-    print_all_classrooms(cout, classrooms);
-
-    //students_ini(students, sys);
-
+    
+    //classrooms_read("classrooms.txt", classrooms, sys);
+    
+    //print_all_classrooms(cout, classrooms);
+    
+    students_ini(students, sys);
+    
     //print_all_students(cout, students);
-
-	//Request a course from the Faculty Of Undergraduate Studies
-	
+    
+    for (int i = 0; i < 20; i++) {
+        FUS.push_back(students[i].bestChoice(courses));
+    }
     return 0;
 }
